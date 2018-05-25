1a64c3fb7f3df42490ebc579f752ae52eec11b53
Jhonatan Iberico <jhonatanibericom@gmail.com>
2018-05-25 12:06:58 -0500

    cambios

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -20,7 +20,7 @@ class Home extends CI_Controller {
       $data['msj']   = null;
       try {
        $nombre = $this->input->post('nombre');
-       $dni = $this->input->post('dni');
+       $dni    = $this->input->post('dni');
        $correo = $this->input->post('correo');
        $celular = $this->input->post('celular');
        $mensaje = $this->input->post('mensaje');
@@ -38,66 +38,19 @@ class Home extends CI_Controller {
        $this->email->to();
        $this->email->subject('Gracias por contactarte con American Latin Travel');
        $texto = '<!DOCTYPE html>
-                <html>
-                    <body>
-                        <table width="500px" cellpadding="0" cellspacing="0" align="center" style="border: solid 1px #ccc;">
-                            <tr>
-                                <td>
-                                    <table width="500" cellspacing="0" cellpadding="0" border="0" align="center" style="background-color: #415564;padding: 10px 20px;">
-                                        <tr>
-                                            <td>
-                                                <table>
-                                                    <tr>
-                                                        <td><img src="http://test.brainblue.com/HPE_promo_made_simple/public/img/logo/logo_header.png" width="120" alt="alternative text" border="0" style="display: block;"></td>
-                                                        <td></td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                            <td>
-                                                <table cellspacing="0" cellpadding="0" border="0" align="right">
-                                                    <tr>
-                                                        <td><font style="font-family: arial;color: #FFFFFF;font-weight: 600;">Promo Made Simple</font></td>
-                                                    </tr>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <table width="400" cellspacing="0" cellpadding="0" border="0" align="center" style="padding: 30px 0">
-                                        <tr>
-                                            <td style="text-align: center;padding: 0;margin: 0;"><font style="font-family: arial;color: #000000;font-size: 18px;font-weight: 600">Promociones por vencer</font></td>
-                                        </tr>
-                                        <tr>
-                                            <td style="padding: 20px 0;">
-                                                <table width="450" cellspacing="0" cellpadding="0" border="0" align="center" style="padding: 20px;">
-                                                    <thead>
-                                                        <tr>
-                                                            <th style="border: 1px solid #cccccc;padding: 5px;"><font style="font-family: arial;font-size: 14px;">Category</font></th>
-                                                            <th style="border: 1px solid #cccccc;padding: 5px;"><font style="font-family: arial;font-size: 14px;">Efective Date</font></th>
-                                                            <th style="border: 1px solid #cccccc;padding: 5px;"><font style="font-family: arial;font-size: 14px;">End Date</font></th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-                                                       '.$html[0].'
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="text-align: center;padding-bottom: 20px"><a href="http://test.brainblue.com/HPE_promo_made_simple/Home" target="_blank" style="font-family: arial;color: #00B388;font-size: 14px; text-decoration: underline;font-weight: 600;">Regresar al portal</a></td>
-                                        </tr>
-                                        <tr>
-                                            <td style="text-align: center;"><font style="font-family: arial;color: #D3D3D3;font-size: 12px;">&copy;2018 Hewlett Packard Enterprise Development LP</font></td>
-                                        </tr>
-                                    </table>
-                                </td>
-                            </tr>
-                        </table>
-                    </body>
-                </html>';
+					<html>
+					<head>
+						<title>Latin travel</title>
+					</head>
+					<body>
+						<h1>Datos personales</h1>
+						<p>'.$nombre.'</p>
+						<p>'.$dni.'</p>
+						<p>'.$correo.'</p>
+						<p>'.$celular.'</p>
+						<p>'.$mensaje.'</p>
+					</body>
+				</html>';
        $this->email->message($texto);
        $this->email->send();
        $data['error'] = EXIT_SUCCESS;
