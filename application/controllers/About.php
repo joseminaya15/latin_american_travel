4e954f577c0dacf126aca07f9cfd3287bd788cc6
joseminaya15 <jose.minayac15@gmail.com>
2018-06-04 06:32:06 -0500

    cambios

---
+++ application/controllers/About.php
@@ -0,0 +1,106 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class About extends CI_Controller {
+
+	function __construct() {
+        parent::__construct();
+        $this->load->helper("url");//BORRAR CACHÉ DE LA PÁGINA
+        $this->load->model('M_datos');
+        $this->output->set_header('Last-Modified:'.gmdate('D, d M Y H:i:s').'GMT');
+        $this->output->set_header('Cache-Control: no-store, no-cache, must-revalidate');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0',false);
+        $this->output->set_header('Pragma: no-cache');
+    }
+
+	public function index(){
+    $html  = '';
+    $cont  = 1;
+    $activ = '';
+    $datos = $this->M_datos->getComentarios();
+    if(count($datos) == 0){
+      return;
+    }else {
+      foreach ($datos as $key) {
+        if($cont <= 2){
+          $activ = 'active';
+        }else if($cont > 2){
+          $activ = '';
+        }
+        $foto = ($key->Foto == 'nouser') ? RUTA_IMG.'logo/nouser.jpg' : $key->Foto;
+        $html .= '<div class="owl-item '.$activ.'" style="width: 563px;">
+                    <div class="item">
+                        <div class="mdl-experiencia">
+                            <div class="js-experiencia--perfil">
+                                <img src="'.$foto.'" alt="">
+                            </div>
+                            <div class="js-experiencia--contenido">
+                                <h2>'.$key->Nombre.' '.$key->Apellido.'</h2>
+                                <p>'.$key->comentario.'</p>
+                                <small>'.$key->fecha_coment.'</small>
+                            </div>
+                        </div>
+                    </div>
+                  </div>';
+        $cont++;
+      }
+    }
+    $data['comentarios'] = $html;
+		$this->load->view('v_home', $data);
+	}
+  function comentar(){
+    $data['error'] = EXIT_ERROR;
+    $data['msj']   = null;
+    try {
+      $html  = '';
+      $cont  = 1;
+      $activ = '';
+      $texto    = $this->input->post('comentario');
+      $name     = $this->input->post('name');
+      $lastname = $this->input->post('lastname');
+      $imageUrl = $this->input->post('imageUrl');
+      $email    = $this->input->post('email');
+      $arrayInsert = array('comentario' => $texto,
+                           'Nombre'   => $name,
+                           'Apellido' => $lastname,
+                           'Email'    => $email,
+                           'pass'     => '',
+                           'Foto'     => $imageUrl,
+                           'fecha'    => date("Y-m-d"));
+      $datoInsert  = $this->M_datos->insertarDatos($arrayInsert, 'opiniones');
+      $datos = $this->M_datos->getComentarios();
+      if(count($datos) == 0){
+        return;
+      }else {
+        foreach ($datos as $key) {
+          if($cont <= 2){
+            $activ = 'active';
+          }else if($cont > 2){
+            $activ = '';
+          }
+          $foto = ($key->Foto == 'nouser') ? RUTA_IMG.'logo/nouser.jpg' : $key->Foto;
+          $html .= '<div class="owl-item '.$activ.'" style="width: 563px;">
+                      <div class="item">
+                          <div class="mdl-experiencia">
+                              <div class="js-experiencia--perfil">
+                                  <img src="'.$foto.'" alt="">
+                              </div>
+                              <div class="js-experiencia--contenido">
+                                  <h2>'.$key->Nombre.' '.$key->Apellido.'</h2>
+                                  <p>'.$key->comentario.'</p>
+                                  <small>'.$key->fecha_coment.'</small>
+                              </div>
+                          </div>
+                      </div>
+                    </div>';
+          $cont++;
+        }
+      }
+      $data['comentarios'] = $html;
+      $data['error'] = EXIT_SUCCESS;
+    }catch(Exception $e){
+      $data['msj'] = $e->getMessage();
+    }
+    echo json_encode($data);
+  }
+}
