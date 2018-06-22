ed6c0361f44004764b02d6cf4a427b346cddc052
joseminaya15 <jose.minayac15@gmail.com>
2018-06-22 06:33:20 -0500

    terminado

--- application/controllers/es/Offer.php
+++ application/controllers/es/Offer.php
@@ -14,43 +14,8 @@ class Offer extends CI_Controller {
     }
 
 	public function index(){
-
-        $ofertas = $this->M_datos->getOfertasByBusqueda();
-        
-        $nav = "";
-        $section = "";
-        $opacity = "";
-        $navActive = "active";
-        foreach ($ofertas as $key) {
-            $nav.= '<a id="menu-oferta'.$key->id.'" class="mdl-navigation__link '.$navActive.'" onclick="goToMenu(this.id)">
-                        '.$key->titulo.'
-                    </a>';
-            $navActive = "";
-            $lugares = str_replace(",","-",$key->atractivos);
-            $array_lugares = explode('|',$key->lugar_detalle);
-            $htmlLugares = "";
-            // log_message('error',print_r($array_lugares,true));
-            foreach($array_lugares as $lug) {
-                $array_lug = explode('*', $lug);
-                $htmlLugares.= "<h2>".$array_lug[0]."</h2><p>".$array_lug[1]."</p>";
-            }
-            $section.= '<section id="section-menu-oferta'.$key->id.'" class="section js-paquete js-section--menu '.$opacity.'">
-                            <div class="js-container">
-                                <h2 class="js-title">'.$key->titulo.'</h2>
-                                <p>'.$lugares.'</p>
-                                <p>'.$key->dias.'</p>
-                                <div class="js-paquete--contenido">
-                                    '.$htmlLugares.'
-                                </div>
-                            </div>
-                        </section>';
-            $opacity = "js-opacity-done";
-        }
-        $data['nav'] = $nav;
-        $data['section'] = $section;
-        // 
-
-        
+        $htmlOfer  = __buildCardsOfertas(null, 1);
+        $data['ofertas'] = $htmlOfer;
 		$this->load->view('es/v_offer',$data);
 	}
 }
